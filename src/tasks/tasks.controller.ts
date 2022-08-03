82a2200cbee5b60456fd1eb1afe3922ac9afa572
alishkeir <alishkeir_@hotmail.com>
2022-08-03 20:41:07 +0300

    Added Creating Task From The Tasks Service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
 import { Task } from './task.module';
 import { TasksService } from './tasks.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,5 +11,11 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
-  
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
