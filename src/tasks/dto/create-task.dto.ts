5e2ab44a4cef05e364b99a34415cf78ade3a209a
alishkeir <alishkeir_@hotmail.com>
2022-08-04 15:53:52 +0300

    Added Validation on Creating Task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 //* Data Transfer Object user to specify data type if it's being used in many places
 //* Makes it easy to change data type in one place only
 export class CreateTaskDTO {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
