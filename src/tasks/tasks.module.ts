4c6309de6351c3372c1b696ab0cd3608a1d676f7
alishkeir <alishkeir_@hotmail.com>
2022-08-04 20:22:38 +0300

    Created Tasks Repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,3 +1,5 @@
+import { TasksRepository } from './tasks.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
@@ -5,5 +7,6 @@ import { TasksService } from './tasks.service';
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
 })
 export class TasksModule {}
