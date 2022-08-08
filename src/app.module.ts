4af6a9da94b65d8e9bdb736c2b66b1f63878bc29
alishkeir <alishkeir_@hotmail.com>
2022-08-08 19:14:56 +0300

    Added Validation Config schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,18 @@
+import { ConfigValidationSchema } from './config.schema';
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { async } from 'rxjs';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
+    ConfigModule.forRoot({
+      envFilePath: [`${process.env.NODE_ENV}.env`],
+      validationSchema: ConfigValidationSchema,
+    }),
+
     TasksModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'postgres',
-    //   database: 'task-manager',
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
