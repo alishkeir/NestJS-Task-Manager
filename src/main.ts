6d45afde9ded75602e784bf524b46366210fa88c
alishkeir <alishkeir_@hotmail.com>
2022-08-08 19:46:35 +0300

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
